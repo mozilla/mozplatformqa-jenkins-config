ac9ef9da6d98bd9bcdb49f955495df086045d2e1
Syd Polk <spolk@mozilla.com>
2014-09-18 15:11:59 -0500

    Add email publisher.

--- update_all.tcl
+++ update_all.tcl
@@ -1,5 +1,9 @@
 #!/usr/bin/tclsh
 
+set fd [open email.snippet]
+set snippet [read $fd]
+close $fd
+
 set job_files [glob jobs/*/config.xml]
 
 foreach file $job_files {
@@ -9,14 +13,23 @@ foreach file $job_files {
     set contents [read $fd]
     close $fd
 
-    if {[regsub {[ \t]*<hudson\.plugins\.girls\.CordellWalkerRecorder.*/hudson\.plugins\.girls\.CordellWalkerRecorder>\n} $contents {} newcontents]} {
-        set fd [open $file w]
-        puts $fd $newcontents
-        puts -nonewline "\nReplaced..."
-        close $fd
+    if {[regexp {ExtendedEmailPublisher} $contents match]} {
+        puts -nonewline "already there..."
+    } else {
+        set newcontents $contents
+        if {[regsub {(<publishers>.*)(\n[\s]*</publishers>)} $newcontents \\1\n$snippet\\2 newcontents]} {
+            puts -nonewline "existing publishers..."
+        }
+        if {[regsub {<publishers/>} $newcontents "<publishers>\n$snippet\n  </publishers>" newcontents]} {
+            puts -nonewline "blank publishers..."
+        }
+        if {![string equal $contents $newcontents]} {
+            set fd [open $file w]
+            puts -nonewline $fd $newcontents
+            puts -nonewline "Replaced..."
+            close $fd
+        }
     }
 
-
     puts "done"
-
 }
