7a6a6465a99b2a9bf3964f90b97e036992857b99
Syd Polk <spolk@mozilla.com>
2015-09-03 19:59:12 -0700

    Scripts to enable and disable without having to bring Jenkins down.

---
+++ enable_jobs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import argparse
+import jenkins
+import sys
+
+parser = argparse.ArgumentParser(description='Enable jobs in a jenkins instance')
+parser.add_argument('--regexp', required=True)
+parser.add_argument('--jenkins-host', required=True, dest='host')
+args = parser.parse_args(sys.argv[1:])
+
+jenkins_instance = jenkins.Jenkins(args.host)
+jobs = jenkins_instance.get_job_info_regex(args.regexp)
+for job in jobs:
+    name = job['name']
+    if 'beta' in name:
+        print 'Skipping %s...' % name
+    else:
+        print 'Enabling %s...' % name
+        jenkins_instance.enable_job(name)
+
+print 'Done.'
+
+
